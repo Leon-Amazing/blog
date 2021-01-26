ad3697afe8daca95e58ced8df78a54684441cb98
Leon-Amazing <leon1787128919@163.com>
2021-01-26 19:20:18 +0800

    leon's first blog

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# Hello VuePress!
+---
+home: true
+heroText: 'Leon-Amazing'
+tagline: 'The world kisses me bitterly, wants me to sing back'
+bgImageStyle: {
+  height: '100vh',
+  color: '#fff'
+}
+isShowTitleInHome: true 
+---
+
+<!-- 首页 -->
--- deploy.sh
+++ deploy.sh
@@ -4,22 +4,12 @@
 set -e
 
 # 生成静态文件
-npm run docs:build
+npm run build
 
 # 进入生成的文件夹
-cd docs/.vuepress/dist
+cd .vuepress/dist
 
-# 如果是发布到自定义域名
-# echo 'www.example.com' > CNAME
-
-git init
-git add -A
+git status
+git add .
 git commit -m 'deploy'
-
-# 如果发布到 https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
-
-# 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:Leon-Amazing/blog.git master:gh-pages
-
-cd -
+git push
